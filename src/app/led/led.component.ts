2d5e25a6e455ef74b059f892bf8c142a51185337
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-01-05 22:51:09 +0100

    Success connection to websockets in led component

--- src/app/led/led.component.ts
+++ src/app/led/led.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-led',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./led.component.css']
 })
 export class LedComponent implements OnInit {
+  socket: SocketIOClient.Socket;
 
-  constructor() { }
+  constructor() {
+    this.socket = io.connect();
+  }
 
   ngOnInit() {
+    this.socket.emit('event1', {
+      msg: 'Client to server, can u hear me server?'
+    });
+    this.socket.on('event2', (data: any) => {
+      console.log(data.msg);
+      this.socket.emit('event3', {
+        msg: 'Yes, its working for me!'
+      });
+      this.socket.on('event4', (_data: any) => {
+        console.log(_data.msg);
+      });
+    });
   }
 
 }
